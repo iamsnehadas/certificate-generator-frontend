06ace5c0c11fb94d0b0ec766bbf440a25c4dcbcc
iamsnehadas <snehasnow8@gmail.com>
2024-12-01 00:49:21 +0530

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://certificate-generator-backend.onrender.com/api", 
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api/certificates", 
 });
 
 export default api;
